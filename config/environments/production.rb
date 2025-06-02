85884f11bb06c5ac458fc0a549b661c2e022cd23
Matheus Ferreira <matheusmlfg@gmail.com>
2025-06-02 23:48:05 +0000

    Removes prod secret key from prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  Rails.application.config.secret_key_base = Rails.application.credentials.secret_key_base
 end
